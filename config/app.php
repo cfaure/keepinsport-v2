bb9b1c9d066137d24f9305e744786d1e297e7375
cfaure <clement.faure@gmail.com>
2016-11-17 00:11:59 +0100

    Few modifications to default config.

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' =>  env('APP_NAME', 'Keepinsport'),
 
     /*
     |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost/keepinsport-v2/public'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Keepinsport
                 </div>
 
                 <div class="links">
